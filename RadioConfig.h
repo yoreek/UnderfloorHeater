1b2ea11d2dbe38a20a8f0e5bd3ab857bac8b3773
Yuriy Ustushenko <yoreek@yahoo.com>
2016-11-20 21:48:36 +0200

    convert symlink to RadioConfig.h to real file

--- RadioConfig.h
+++ RadioConfig.h
@@ -1,1 +1,20 @@
-../SmartHouse/RadioConfig.h
+#pragma once
+#ifndef _RADIO_CONFIG_H_
+#define _RADIO_CONFIG_H_
+
+#include <RF24.h>
+
+#define RADIO_SERVER_ADDR      0x7878787878LL
+#define RADIO_CHILD_BASE_ADDR  0xA3A3A3A300LL
+#define RADIO_BEDROOM_ADDR     (RADIO_CHILD_BASE_ADDR | 1)
+#define RADIO_BATHROOM_ADDR    (RADIO_CHILD_BASE_ADDR | 2)
+#define RADIO_MAX_PIPES        6
+
+#define RADIO_PA_LEVEL         RF24_PA_MIN
+#define RADIO_DATA_RATE        RF24_1MBPS
+#define RADIO_CHANNEL          33  //Pick a channel to use between 0 nd 70
+#define RADIO_IRQ_PIN          2
+#define RADIO_CE_PIN           9
+#define RADIO_CS_PIN           10
+
+#endif
