40e8bd3ec0655433e82efe09da0179c506a502ef
Yuriy Ustushenko <yoreek@yahoo.com>
2016-11-20 23:53:00 +0200

    Fix bug with private method flush_rx()

--- MyMulticeiver.cpp
+++ MyMulticeiver.cpp
@@ -10,7 +10,13 @@ void MyMulticeiver::maintain() {
         receiver = get(pipeNo);
         if (receiver == NULL) {
             DEBUG("Can't find receiver for pipe No: %d", (int) pipeNo);
+#ifdef RF24_TUNED
             _radio->flush_rx();
+#else
+            char buf;
+            while (_radio->available())
+                _radio->read(&buf, sizeof(char));
+#endif
         }
         else {
             DEBUG("Found receiver: %s", receiver->name());
